231774686a5e61426dd08803a39d6d970c6a6f41
arrehman19 <abdurrehmanirfan94@gmail.com>
2025-12-27 11:08:56 +0500

    second commit

--- app.js
+++ app.js
@@ -5,10 +5,63 @@
 //     skill: ["Web Developer", "HTML", "CSS", "JS"]
 // };
 
-let myWindow ={
-myAlert : function() {
-    alert("mai chla");
-    return "kuch bhi";
-}};
+// let myWindow ={
+// myAlert : function() {
+//     alert("mai chla");
+//     return "kuch bhi";
+// }};
 
-console.log(myWindow.myAlert());
+// console.log(myWindow.myAlert());
+
+// let mobilePkg ={
+//     basic : {
+//         price : 200,
+//         duration : "monthly",
+//         data : "2GB",
+//     },
+//     professional : {
+//          price : 2000,
+//         duration : "monthly",
+//         data : "20GB"
+//     },
+//     ultimate : {
+//          price : 5000,
+//         duration : "monthly",
+//         data : "200GB"
+//     }
+// };
+// console.log(mobilePkg.basic.price);
+// alert("mobile packages mai sirf basic pkg ki ye price hai: " + mobilePkg.basic.price);
+
+// let newObj = {
+    // skills :"HTML",
+    // shift : "shift hojao"
+// }
+
+// newObj.kuchbi = "yeh bhi add hojayega";
+
+
+// console.log(newObj.skills = ["HTML", "CSS", "JS"]);
+// // console.log(newObj.skills.unshift("GitHub"));
+// // newObj.shift();
+
+// console.log(newObj.shift)
+// // console.log(newObj)
+
+
+let studentIdCardGenerator = {
+    studentName: "Abdur Rehman",
+    studentRollNo: "12345",
+    studentAge: 18,
+    studentClass: "12th Grade",
+    studentSchool: "SMIT",
+    studentAddress: "Karachi, Pakistan"
+};
+console.log("Student Name: " + studentIdCardGenerator.studentName);
+console.log("Student Roll No: " + studentIdCardGenerator.studentRollNo);
+console.log("Student Age: " + studentIdCardGenerator.studentAge);
+console.log("Student Class: " + studentIdCardGenerator.studentClass);
+console.log("Student School: " + studentIdCardGenerator.studentSchool);
+console.log("Student Address: " + studentIdCardGenerator.studentAddress);
+
+document.getElementById('name').nnerText = studentIdCardGenerator.studentName;
