17ba7be970ca0cd5b43b2a3434b4bdf5c6a6878d
arrehman19 <abdurrehmanirfan94@gmail.com>
2025-12-27 09:28:14 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+// let obj = {
+//     fullName: "Abdur Rehman ", 
+//     skill: ["Web Developer", "HTML", "CSS", "JS"]
+// };
+
+let myWindow ={
+myAlert : function() {
+    alert("mai chla");
+    return "kuch bhi";
+}};
+
+console.log(myWindow.myAlert());
